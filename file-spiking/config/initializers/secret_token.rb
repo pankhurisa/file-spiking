bc5ca16de654a93f423ad5df9770492174956d2a
Pankhuri <pankhuri@thoughtworks.com>
2010-10-12 14:37:45 +0530

    First commit

---
+++ file-spiking/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FileSpiking::Application.config.secret_token = '12a0743507d0e92bc60dd43eda98fcc5efeb41c909616b3f89714bad59e5c2f532b332e1bfbf5a32143ff9c1f2cdc8ac0e2d2132aad25975590625d0b0ca742b'
